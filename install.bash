2178a21525676755d9e2f81f7fb3328519c27c33
Ash Belmokadem <a.belmokadem@gmail.com>
2018-07-24 11:13:03 +0200

    Remove copying

--- install.bash
+++ install.bash
@@ -27,9 +27,6 @@ sed -i "s/TEMPLATE_DEBUG = DEBUG/TEMPLATE_DEBUG = False/g" dojo/settings/setting
 sed -i "s/DEBUG = True/DEBUG = False/g" dojo/settings/settings.py
 sed -i "s/ALLOWED_HOSTS = \[]/ALLOWED_HOSTS = [$ALLOWED_HOSTS, 'localhost', '$(awk 'END{print $1}' /etc/hosts)']/g" dojo/settings/settings.py
 
-echo "*** Copy static files"
-sudo cp -R /opt/django-DefectDojo/dojo/static /opt/defect-dojo-static
-
 echo "*** Tail nginx logs"
 tail -f /var/log/nginx/* &
 
